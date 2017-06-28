de7f0ba93379ce4170987c7ccb4645a3310c1c0b
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-06-28 03:49:16 -0400

    add words

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-const product = document.getElementById("product");
-const banners = document.getElementById("banners");
-const hashTagSuper = document.getElementById("super");
-const fresh = document.getElementById("fresh");
-const cta = document.getElementById("cta");
+const product = document.getElementsByClassName("cup");
+const banners = document.getElementsByClassName("craft");
+const hashTagSuper = document.getElementsByClassName("super");
+const fresh = document.getElementsByClassName("fresh");
+// const cta = document.getElementById("cta");
 
-// TweenMax.from(product, 2, {left: 500, top: 300});
-// TweenMax.to(product, 2, {left: 400, top: -250, scale: 1.5, delay: 2});
-TweenMax.fromTo(product, 2, {left: 500, top: 300}, {left: 400, top: -250, scale: 1.5, delay: 1});
-TweenMax.to(product, 2, {left: 300, top: -300, scale: .95, delay: 4})
-TweenMax.to(product, 2, {left: 500, top: -350, delay: 6});
-TweenMax.to(product, 2, {top: -150, left: 400, scale: .4, delay: 10});
+//TweenMax.fromTo(product, 2, {left: 500, top: 300}, {left: 400, top: -250, scale: 1.5, delay: 1});
+TweenMax.to(product, 2, {left: -115, top: -245, scale: 1.1, delay: 2});
+TweenMax.to(product, 2, {left: -150, top: -300, scale: .7, delay: 5})
+TweenMax.to(product, 2, {left: 0, top: -310, delay: 6});
+TweenMax.to(product, 2, {top: -200, left: -100, scale: .25, delay: 11});
 
-TweenMax.to(banners, 2, {opacity: 1, delay: 7});
+TweenMax.to(banners, 2, {opacity: 1, delay: 8});
 TweenMax.to(banners, 2, {opacity: 0, delay: 10});
 
-TweenMax.to(hashTagSuper, 2, {opacity: 1, delay: 10});
-TweenMax.to(hashTagSuper, 2, {left: 400, delay: 11});
+TweenMax.to(hashTagSuper, 2, {opacity: 1, delay: 12});
+TweenMax.to(hashTagSuper, 2, {left: 0, delay: 13});
 
-TweenMax.to(fresh, 2, {opacity: 1, delay: 12.5});
+TweenMax.to(fresh, 2, {opacity: 1, delay: 14.5});
 
-TweenMax.to(cta, 2, {opacity: 1, delay: 13});
+// TweenMax.to(cta, 2, {opacity: 1, delay: 13});
