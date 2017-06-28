ff137c41e79f34425abab3d63facf9ff73bfb9f1
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-06-28 00:36:55 -0400

    add words

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const product = document.getElementById("product");
+const banners = document.getElementById("banners");
+const hashtagSuper = document.getElementById("super");
+
 TweenMax.from(product, 2, {left: 100, top: 300});
 TweenMax.to(product, 2, {left: 0, top: -100, scale: 1.5});
 TweenMax.to(product, 2, {right: 10, top: -300, scale: .95, delay: 2})
 TweenMax.to(product, 2, {left: 400, delay: 4});
-//TweenMax.to(product, 2, {top: 200});
+TweenMax.to(product, 2, {top: -100, left: 400, scale: .5, delay: 7});
 
-var tl = new TimelineLite();
-//tl.to(product, 1, {left: 100})
+TweenMax.to(banners, 2, {opacity: 1, delay: 5});
+TweenMax.to(banners, 2, {opacity: 0, delay: 7});
 
-// tl.to( product , 3 , {top: -2, left: -10, ease:Sine.easeInOut } )
-// 	.to( product, .25, {top: 0, left: -390, ease:Sine.easeInOut } )
-// 	.to( product, 3, {top: -5, left: -395, ease:Sine.easeInOut } )
-// 	.to( product, .25, {top: -300, left: 0, ease:Sine.easeInOut } )
-// 	.to( product, 3, {top: -303, left: -10, ease:Sine.easeInOut } )
-// 	.to( product, .25, {top: -300, left: -390, ease:Sine.easeInOut } )
-// 	.to( product, 3, {top: -304, left: -394, ease:Sine.easeInOut } )
-// ;
+TweenMax.to(hashtagSuper, 2, {opacity: 1, delay: 7});
+TweenMax.to(hashtagSuper, 2, {left: 400, delay: 8});
